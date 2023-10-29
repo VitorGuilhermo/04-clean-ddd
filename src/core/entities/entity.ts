b1065bc797f752efe583884705e73d62a22b4384
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-10-29 18:33:44 -0300

    add: interface entity and id

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,16 @@
+import { UniqueEntityId } from "./unique-entity-id"
+
+export class Entity<Props> {
+    private _id: UniqueEntityId
+    protected props: any
+
+
+    constructor(props: Props, id?: string) {
+        this.props = props
+        this._id = new UniqueEntityId(id)
+    }
+
+    get id() {
+        return this._id
+    }
+}
