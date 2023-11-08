394ac3fc06466b73b78d6d3d03c3205ed470ce9b
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-11-08 20:31:57 -0300

    add: create question use case

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -6,7 +6,7 @@ const fakeAnswersRepository: AnswersRepository = {
     create: async (answer: Answer) => {},
 }
 
-test('create ab answer', async () => {
+test('create a answer', async () => {
     const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
     const answer = await answerQuestion.execute({
