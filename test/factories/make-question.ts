eaa04425d971c9560759f412b2cb75f0d88eb431
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-11-09 21:03:31 -0300

    add: faker generator info

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,15 +1,16 @@
+import { faker } from "@faker-js/faker"
+
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(override: Partial<QuestionProps> = {}, id?: UniqueEntityId) {
     const question = Question.create({
         authorId: new UniqueEntityId('1'),
-        title: 'Nova pergunta',
-        slug: Slug.create('nova-pergunta'),
-        content: 'Conteúdo da pergunta',
+        title: faker.lorem.sentence(),
+        content: faker.lorem.text(),
         ...override
-    })
+    }, id)
 
     return question
 }
