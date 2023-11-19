dd98c29edd023ee062df9e77b343bc7cec723a40
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-11-19 00:21:31 -0300

    chosen best answer notification

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -5,6 +5,7 @@ import { Optional } from "@/core/types/optional"
 import dayjs from "dayjs"
 import { QuestionAttachment } from "./question-attachment"
 import { QuestionAttachmentList } from "./question-attachment-list"
+import { QuestionBestAnswerChosenEvent } from "../events/question-best-answer-chosen-event"
 
 export interface QuestionProps {
     authorId: UniqueEntityId
@@ -81,7 +82,11 @@ export class Question extends AggregateRoot<QuestionProps> {
     }
 
     set bestAnswerId(bestAnswerId: UniqueEntityId) {
+        if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
+            this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
+          }
         this.props.bestAnswerId = bestAnswerId
+        
         this.touch()
     }
 
